cc8f947eea257fdc170d2d84ac584a1ade4f9160
Sarvesvar Srinivas <115775080+Sarvesvar29@users.noreply.github.com>
2024-09-17 00:46:24 +0530

    Client.py

--- client.py
+++ client.py
@@ -4,7 +4,6 @@
 import socket
 import threading
 
-# Function to handle receiving messages from the server
 def receive_messages():
     while True:
         try:
@@ -16,22 +15,20 @@ def receive_messages():
             print("An error occurred!")
             client.close()
             break
-
-# Function to handle sending messages to the server
+            
 def send_messages():
     while True:
         message = input("")
         client.send(message.encode('utf-8'))
 
 # Client setup
-SERVER = "127.0.0.1"  # Server IP (localhost for testing)
+SERVER = "127.0.0.1"  
 PORT = 5555
 ADDRESS = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDRESS)
 
-# Start threads for receiving and sending messages
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
 
